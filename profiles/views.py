4aba53a2e612cdf5d3cf6d24a3a84b49043acf19
RobWar-code <rowaniar@aol.com>
2023-07-10 18:34:02 +0000

    convert profiles and posts views to generics

--- profiles/views.py
+++ profiles/views.py
@@ -1,42 +1,17 @@
 from django.http import Http404
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework import status, generics
 from .models import Profile
 from .serializers import ProfileSerializer
 from drf_api.permissions import IsOwnerOrReadOnly
 
-class ProfileList(APIView):
-    def get(self, request):
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, context={'request': request}, many=True)
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
 
-class ProfileDetail(APIView):
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
     # Add an input form derived from the model
     serializer_class = ProfileSerializer
     # Add the permissions features
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        try:
-            profile = Profile.objects.get(pk=pk)
-            # Add this to check the permissions, using the IsOwnerOrReadOnly object
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request}, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
-        
+    queryset = Profile.objects.all()
